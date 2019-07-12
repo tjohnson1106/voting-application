12979a1f76594125688b184f0bf257a84cfe947b
Thomas Johnson <tjohnson1106@gmail.com>
2019-07-12 16:44:45 -0400

    created choose poll option function

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const fs = require("fs");
+const bodyParser = require("body-parser");
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
@@ -14,8 +22,34 @@ app.get("/poll", (req, res) => {
 });
 
 app.post("/vote/new", (req, res) => {
-  console.log(req.body);
+  if (req.body.linux === "on") {
+    choosePollOption(req, res, "linux");
+  } else if (req.body.macos === "on") {
+    choosePollOption(req, res, "macos");
+  } else if (req.body.windows === "on") {
+    choosePollOption(req, res, "windows");
+  } else {
+    res.redirect("/?incorrect+input");
+  }
 });
+
+function choosePollOption(req, res, topic) {
+  let poll = {};
+  fs.readFile(__dirname + "/poll.json", "utf8", function(err, data) {
+    poll = JSON.parse(data);
+    poll[topic] += 1;
+    console.log(poll);
+    fs.writeFile(__dirname + "/poll.json", JSON.stringify(poll), function(
+      err,
+      data
+    ) {
+      console.log(err);
+      console.log(data);
+      res.status(200).send(`want to <a href="/">vote<a/> again`);
+    });
+  });
+  console.log(poll);
+}
 
 app.listen(3001, () => {
   console.log("listening on port 3001");
